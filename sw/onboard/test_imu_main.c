da8a2cbd13183bb6207c564542a60b9699894774
John Stowers <john.stowers@gmail.com>
2009-06-16 16:21:14 +1200

    test_imu_main works

--- sw/onboard/test_imu_main.c
+++ sw/onboard/test_imu_main.c
@@ -19,13 +19,14 @@ static inline void main_event_task( void );
 static inline void on_imu_event(void);
 
 int main( void ) {
-  main_init();
-  while(1) {
-    if (sys_time_periodic())
-      main_periodic_task();
-    main_event_task();
-  }
-  return 0;
+    main_init();
+    while(1)
+    {
+        if (sys_time_periodic())
+            main_periodic_task();
+        main_event_task();
+    }
+    return 0;
 }
 
 static inline void main_init( void ) {
@@ -42,49 +43,64 @@ static inline void main_init( void ) {
 }
 
 static inline void main_periodic_task( void ) {
-  #if 0
-  RunOnceEvery(100, {
-    led_toggle(3);
-    DOWNLINK_SEND_ALIVE(16, MD5SUM);
-  });
-  #endif
-  booz2_imu_periodic();
+    comm_periodic_task(COMM_1);
 
+    RunOnceEvery(100, {
+        led_toggle(3);
+    });
+
+    booz2_imu_periodic();
 }
 
-static inline void main_event_task( void ) {
+static inline void main_event_task( void )
+{
+    comm_event_task(COMM_1);
 
-  Booz2ImuEvent(on_imu_event);
-
+    Booz2ImuEvent(on_imu_event);
+    
+    Booz2ImuSpiEvent(booz2_max1168_read);
 }
 
-static inline void on_imu_event(void) {
-  Booz2ImuScaleGyro();
-  Booz2ImuScaleAccel();
+#define TICKS 30
+static inline void on_imu_event(void)
+{
+    static uint8_t cnt;
 
-  led_toggle(2);
-#if 0
-  static uint8_t cnt;
-  cnt++;
-  if (cnt > 15) cnt = 0;
+    Booz2ImuScaleGyro();
+    Booz2ImuScaleAccel();
 
-  if (cnt == 0) {
-    DOWNLINK_SEND_IMU_GYRO_RAW(&booz_imu.gyro_unscaled.p,
-			       &booz_imu.gyro_unscaled.q,
-			       &booz_imu.gyro_unscaled.r);
-    
-    DOWNLINK_SEND_IMU_ACCEL_RAW(&booz_imu.accel_unscaled.x,
-				&booz_imu.accel_unscaled.y,
-				&booz_imu.accel_unscaled.z);
-  }
-  else if (cnt == 7) {
-    DOWNLINK_SEND_BOOZ2_GYRO(&booz_imu.gyro.p,
-			     &booz_imu.gyro.q,
-			     &booz_imu.gyro.r);
-    
-    DOWNLINK_SEND_BOOZ2_ACCEL(&booz_imu.accel.x,
-			      &booz_imu.accel.y,
-			      &booz_imu.accel.z);
-  }  
-#endif
+    if (++cnt > TICKS) cnt = 0;
+
+    if (cnt == 0)
+    {
+        led_on(2);
+
+        MESSAGE_SEND_IMU_GYRO_RAW(
+                COMM_1,
+                &booz_imu.gyro_unscaled.p,
+                &booz_imu.gyro_unscaled.q,
+                &booz_imu.gyro_unscaled.r);
+
+        MESSAGE_SEND_IMU_ACCEL_RAW(
+                COMM_1,
+                &booz_imu.accel_unscaled.x,
+			    &booz_imu.accel_unscaled.y,
+			    &booz_imu.accel_unscaled.z);
+    }
+    else if (cnt == (TICKS / 2))
+    {
+        led_off(2);
+
+        MESSAGE_SEND_WASP_GYRO(
+                COMM_1,
+                &booz_imu.gyro.p,
+                &booz_imu.gyro.q,
+                &booz_imu.gyro.r);
+
+        MESSAGE_SEND_WASP_ACCEL(
+                COMM_1,
+                &booz_imu.accel.x,
+                &booz_imu.accel.y,
+                &booz_imu.accel.z);
+    }
 }
