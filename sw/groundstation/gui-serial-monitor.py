279c81183973632a90647406298c593707cb35ab
John Stowers <john.stowers@gmail.com>
2009-07-02 15:13:34 +1200

    extract gobject source from gui mon

--- sw/groundstation/gui-serial-monitor.py
+++ sw/groundstation/gui-serial-monitor.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import gobject
 import gtk
 
 import os.path
@@ -9,45 +8,14 @@ import optparse
 import ppz
 import ppz.transport as transport
 import ppz.messages as messages
+import ppz.monitor as monitor
 
 import ppz.ui.treeview as treeview
 import ppz.ui.senders as senders
 
-class GObjectSerialMonitor(gobject.GObject):
-
-    __gsignals__ = {
-        "got-message" : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, [
-            gobject.TYPE_PYOBJECT]),
-        }
-
-    def __init__(self, serialsender):
-        gobject.GObject.__init__(self)
-        self._watch = None   
-
-        serialsender.connect("serial-connected", self._on_serial_connected)
-        serialsender.connect_to_port()
-
-    def _on_serial_connected(self, serial, connected):
-        #remove the old watch
-        if self._watch:
-            gobject.source_remove(self._watch)
-
-        if connected:
-            #add new watch
-            self._watch = gobject.io_add_watch(
-                            serial.get_serial().fileno(), 
-                            gobject.IO_IN | gobject.IO_PRI,
-                            self.on_serial_data_available,
-                            serial,
-                            priority=gobject.PRIORITY_HIGH
-            )
-
-    def on_serial_data_available(self, fd, condition, serial):
-        raise NotImplementedError
-
-class UI(GObjectSerialMonitor):
+class UI(monitor.GObjectSerialMonitor):
     def __init__(self, messages_file, serialsender, transport, debug=False):
-        GObjectSerialMonitor.__init__(self, serialsender)
+        monitor.GObjectSerialMonitor.__init__(self, serialsender)
         self._debug = debug
         self._transport = transport
 
---
+++ sw/groundstation/ppz/monitor.py
@@ -0,0 +1,34 @@
+import gobject
+
+class GObjectSerialMonitor(gobject.GObject):
+
+    __gsignals__ = {
+        "got-message" : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, [
+            gobject.TYPE_PYOBJECT]),
+        }
+
+    def __init__(self, serialsender):
+        gobject.GObject.__init__(self)
+        self._watch = None   
+
+        serialsender.connect("serial-connected", self._on_serial_connected)
+        serialsender.connect_to_port()
+
+    def _on_serial_connected(self, serial, connected):
+        #remove the old watch
+        if self._watch:
+            gobject.source_remove(self._watch)
+
+        if connected:
+            #add new watch
+            self._watch = gobject.io_add_watch(
+                            serial.get_serial().fileno(), 
+                            gobject.IO_IN | gobject.IO_PRI,
+                            self.on_serial_data_available,
+                            serial,
+                            priority=gobject.PRIORITY_HIGH
+            )
+
+    def on_serial_data_available(self, fd, condition, serial):
+        raise NotImplementedError
+
