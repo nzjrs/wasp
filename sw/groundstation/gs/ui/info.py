5d3538c6287dc8f73a6654f766da2848ed7ca89f
John Stowers <john.stowers@gmail.com>
2009-07-02 15:42:47 +1200

    as

--- sw/groundstation/gs/ui/info.py
+++ sw/groundstation/gs/ui/info.py
@@ -3,6 +3,9 @@ import gtk
 
 import gs.ui
 
+def set_image_from_file(image, filename):
+    pass
+
 class InfoBox(gs.ui.GtkBuilderWidget):
     def __init__(self):
         me = os.path.abspath(__file__)
@@ -13,6 +16,7 @@ class InfoBox(gs.ui.GtkBuilderWidget):
 
     def set_build_info(self, rev, branch, target, dirty, time):
         self.get_resource("rev_value").set_text(rev)
+
 
 if __name__ == "__main__":
     i = InfoBox()
