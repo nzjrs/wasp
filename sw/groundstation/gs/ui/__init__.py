a242536fcafbc7bbdb5d8a6b118d6a2b2efd129d
John Stowers <john.stowers@gmail.com>
2009-07-02 13:56:41 +1200

    info widget

--- sw/groundstation/gs/ui/__init__.py
+++ sw/groundstation/gs/ui/__init__.py
@@ -10,3 +10,23 @@ def make_label(text, width=None):
     l.set_alignment(0.0, 0.5)
     l.set_padding(5, 0)
     return l
+
+class GtkBuilderWidget:
+    def __init__(self, uifile):
+        self._builder = gtk.Builder()
+        self._builder.add_from_file(uifile)
+        self._resources = {}
+
+    def set_instance_resources(self, *resources):
+        for r in resources:
+            setattr(self, "_%s" % r.lower(), self.get_resource(r))
+
+    def get_resource(self, name):
+        if name not in self._resources:
+            w = self._builder.get_object(name)
+            if not w:
+                raise Exception("Could not find widget: %s" % name)
+            self._resources[name] = w
+
+        return self._resources[name]
+
---
+++ sw/groundstation/gs/ui/info.py
@@ -0,0 +1,27 @@
+import os.path
+import gtk
+
+import gs.ui
+
+class InfoBox(gs.ui.GtkBuilderWidget):
+    def __init__(self):
+        me = os.path.abspath(__file__)
+        uifile = os.path.join(os.path.dirname(me), "info.ui")
+        gs.ui.GtkBuilderWidget.__init__(self, uifile)
+
+        self.box = self.get_resource("info_hbox")
+
+    def set_build_info(self, rev, branch, target, dirty, time):
+        self.get_resource("rev_value").set_text(rev)
+
+if __name__ == "__main__":
+    i = InfoBox()
+
+    w = gtk.Window()
+    w.add(i.box)
+    w.show_all()
+    w.connect("delete-event", lambda *a: gtk.main_quit())
+
+    i.set_build_info("123", 0, 0, 0, 0)
+
+    gtk.main()
