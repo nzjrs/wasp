3b5fd50a9dea4fc123807697ac3e6c08c68be238
John Stowers <john.stowers@gmail.com>
2009-07-16 19:57:15 +1200

    tidy some config defines

--- sw/onboard/config/config.h
+++ sw/onboard/config/config.h
@@ -26,33 +26,33 @@
 #define PCLK (CCLK / PBSD_VAL) 
 
 /* Onboard LEDs */
-#define LED_1_BANK 1
-#define LED_1_PIN 25
-#define LED_2_BANK 1
-#define LED_2_PIN 24
-#define LED_3_BANK 1
-#define LED_3_PIN 23
-#define LED_4_BANK 1
-#define LED_4_PIN 31
+#define LED_1_BANK      1
+#define LED_1_PIN       25
+#define LED_2_BANK      1
+#define LED_2_PIN       24
+#define LED_3_BANK      1
+#define LED_3_PIN       23
+#define LED_4_BANK      1
+#define LED_4_PIN       31
 
-#define TIME_LED 1
-#define RC_LED 1
-#define GPS_LED 4
-#define BOOZ2_ANALOG_BARO_LED 2
-#define AHRS_ALIGNER_LED 3
+#define TIME_LED                1
+#define RC_LED                  1
+#define GPS_LED                 4
+#define BOOZ2_ANALOG_BARO_LED   2
+#define AHRS_ALIGNER_LED        3
 
 /* PPM : rc rx on P0.28 ( CAP0.2 ) */
-#define PPM_PINSEL PINSEL1
-#define PPM_PINSEL_VAL 0x02
-#define PPM_PINSEL_BIT 24
+#define PPM_PINSEL      PINSEL1
+#define PPM_PINSEL_VAL  0x02
+#define PPM_PINSEL_BIT  24
 
 /* ADC */
 
 /* pressure : P0.10 AD1.2 */
-#define ANALOG_BARO_PINSEL PINSEL0
-#define ANALOG_BARO_PINSEL_VAL 0x03
-#define ANALOG_BARO_PINSEL_BIT 20
-#define ANALOG_BARO_ADC 1
+#define ANALOG_BARO_PINSEL      PINSEL0
+#define ANALOG_BARO_PINSEL_VAL  0x03
+#define ANALOG_BARO_PINSEL_BIT  20
+#define ANALOG_BARO_ADC         1
 
 /* Micromag on SSP, IMU connector */
 #define MM_SS_PIN   28
@@ -65,12 +65,12 @@
 #define MM_RESET_IOSET IO1SET
 #define MM_RESET_IOCLR IO1CLR
 
-#define MM_DRDY_PIN  30
-#define MM_DRDY_PINSEL PINSEL1
-#define MM_DRDY_PINSEL_BIT 28
-#define MM_DRDY_PINSEL_VAL 2
-#define MM_DRDY_EINT 3
-#define MM_DRDY_VIC_IT VIC_EINT3
+#define MM_DRDY_PIN         30
+#define MM_DRDY_PINSEL      PINSEL1
+#define MM_DRDY_PINSEL_BIT  28
+#define MM_DRDY_PINSEL_VAL  2
+#define MM_DRDY_EINT        3
+#define MM_DRDY_VIC_IT      VIC_EINT3
 
 // damit, we have two of them now
 //#define POWER_SWITCH_LED 3
@@ -88,10 +88,10 @@
 #define PERIODIC_TASK_PERIOD SYS_TICS_OF_SEC((1./512.))
 
 /* Radio Control : Futaba is falling edge clocked whereas JR is rising edge */
-#define RADIO_CONTROL 1
-#define RC_FUTABA 0
-#define RC_JR     1
-#define RADIO_CONTROL_TYPE RC_FUTABA
+#define RADIO_CONTROL       1
+#define RC_FUTABA           0
+#define RC_JR               1
+#define RADIO_CONTROL_TYPE  RC_FUTABA
 
 /* UARTS */
 #define USE_UART0 1
@@ -127,16 +127,16 @@
 #define BOOZ2_FMS_TYPE              BOOZ2_FMS_TYPE_TEST_SIGNAL
 
 /* VIC */
-#define TIMER0_VIC_SLOT 1
-#define ADC0_VIC_SLOT 2
-#define ADC1_VIC_SLOT 3
-#define UART0_VIC_SLOT 5
-#define UART1_VIC_SLOT 6
-#define USB_VIC_SLOT 7
-#define MAX1168_EOC_VIC_SLOT 8
-#define SSP_VIC_SLOT 9
-#define I2C0_VIC_SLOT 10
+#define TIMER0_VIC_SLOT             1
+#define ADC0_VIC_SLOT               2
+#define ADC1_VIC_SLOT               3
+#define UART0_VIC_SLOT              5
+#define UART1_VIC_SLOT              6
+#define USB_VIC_SLOT                7
+#define MAX1168_EOC_VIC_SLOT        8
+#define SSP_VIC_SLOT                9
+#define I2C0_VIC_SLOT               10
 /* #define I2C1_VIC_SLOT 11    AMI601 (Not used) */
-#define MICROMAG_DRDY_VIC_SLOT 11
+#define MICROMAG_DRDY_VIC_SLOT      11
 
 #endif /* CONFIG_BOOZ2_V1_0_H */
