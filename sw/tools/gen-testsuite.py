f13979b621312c2cbc7a8c956e52ff487c987974
Aaron Marburg <amarburg@meriwether.canterbury.ac.nz>
2009-12-16 16:13:42 +1300

    Slightly improved automatic testsuite software which builds sub-main() functions in each source file, then binds them together under a top-level main()

--- sw/tools/gen-testsuite.py
+++ sw/tools/gen-testsuite.py
@@ -4,6 +4,7 @@
 import sys
 import fileinput
 import re
+import os
 
 if __name__ == "__main__":
     EXE = "fgfs"
@@ -15,10 +16,14 @@ if __name__ == "__main__":
     # Process command line args
     for fname in sys.argv[1:]:
 
+	base_filename = os.path.splitext( os.path.split( fname )[1] )[0]
+
 	# Right now this parser will only accept function definitions which are
 	# all on one line.  Which is a pity because fixtured functions tend to be
 	# pretty verbose.  hmm....
 	for line in fileinput.input( fname ):
+	    print line,
+
     	    func = re.search("void\s*(.*)_tc\s*\(([A-Za-z0-9\_,\*\s]*)\)",line)	
 	    if func:
 		if re.search("void", func.group(2) ):
@@ -30,43 +35,18 @@ if __name__ == "__main__":
                         	                  'fixture': values[1] } )
 			fixture_list.append( values[1] )
 
-    print """
-/* Auto-generated test program.  Do not edit */
-#include <glib.h>
-"""
- 
-    for func in simple_list:
-	print "void %s_tc(void);" % func
+        print """
+/* Begin auto-generated test suite */
 
-    for func in fixture_list:
-	print "void %s_setup( %s *, gconstpointer );" % (func, func)
-        print "void %s_teardown( %s *, gconstpointer );" % (func, func)
-
-    for func in fixtured_test_list:
-        print "void %s_tc( %s *, gconstpointer );" % (func['name'], func['fixture'] )
-
-    print """
-int main( int argc, char **argv )
+void main_%s( void )
 {
-        g_test_init( &argc, &argv, NULL );
+""" % (base_filename)
+	
+	for func in simple_list:
+            print "\tg_test_add_func(\"%s\", %s_tc);" % (('/' + func.replace('_','/') ),func)
 
-"""
+        for func in fixtured_test_list:
+            print "\tg_test_add(\"%s\", %s, (gconstpointer)NULL, %s_setup, %s_tc, %s_teardown);" % (('/' + func['name'].replace('_','/') ), func['fixture'], func['fixture'], func['name'], func['fixture'])
 
-    for func in simple_list:
-	print "\tg_test_add_func(\"%s\", %s_tc);" % (('/' + func.replace('_','/') ),func)
-
-    for func in fixtured_test_list:
-	print "\tg_test_add(\"%s\", %s, (gconstpointer)NULL, %s_setup, %s_tc, %s_teardown);" % (('/' + func['name'].replace('_','/') ), func['fixture'], func['fixture'], func['name'], func['fixture'])
-
-    print """
-
-        return g_test_run();
-}
-    """
-
-#    gentools.print_header(H, generatedfrom=settings_path)
-#    print '#include "std.h"\n'
-#    settings.print_typedefs()
-#    settings.print_defines()
-#    settings.print_values()
-#    gentools.print_footer(H)
+	print
+        print "}"
