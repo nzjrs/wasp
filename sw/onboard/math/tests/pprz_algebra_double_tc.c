2a69e7a5e3bbdffb10cf96f219083d21372ab987
Aaron Marburg <aaron@tasman.notetofutureself.org>
2010-01-14 10:37:04 +1300

    Added a few EULER test cases.

--- sw/onboard/math/tests/pprz_algebra_double_tc.c
+++ sw/onboard/math/tests/pprz_algebra_double_tc.c
@@ -39,13 +39,25 @@ static void math_pprz_algebra_double_vect2_fixture_teardown( math_pprz_algebra_d
 	;
 }
 
+/* I feel dirty doing this but it syntactically much nicer than an inline function */
+#ifdef G_ASSERT_A_UNCHANGED
+#undef G_ASSERT_A_UNCHANGED
+#endif
+#define G_ASSERT_A_UNCHANGED G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX, AY )
+
+#ifdef G_ASSERT_B_UNCHANGED 
+#undef G_ASSERT_B_UNCHANGED
+#endif
+#define G_ASSERT_B_UNCHANGED G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY )
+
+
 /*
  * Test functions for the 2-vector functions from pprz_algebra.h
  */
 static void math_pprz_algebra_double_vect2_fixture_tc( math_pprz_algebra_double_vect2_fixture *fix, gconstpointer test_data )
 {
-	G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX, AY );
-	G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY );
+	G_ASSERT_A_UNCHANGED;
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect2_assign_tc( math_pprz_algebra_double_vect2_fixture *fix, gconstpointer test_data )
@@ -56,19 +68,18 @@ static void math_pprz_algebra_double_vect2_assign_tc( math_pprz_algebra_double_v
 
 static void math_pprz_algebra_double_vect2_copy_tc( math_pprz_algebra_double_vect2_fixture *fix, gconstpointer test_data )
 {
-
         VECT2_COPY( fix->a, fix->b );
+	
 	G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, BX, BY );
-	G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY );
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect2_add_tc( math_pprz_algebra_double_vect2_fixture *fix, gconstpointer test_data )
 {
-
         VECT2_ADD( fix->a, fix->b );
 
 	G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX+BX, AY+BY );
-        G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY );
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect2_sub_tc(  math_pprz_algebra_double_vect2_fixture *fix, gconstpointer test_data )
@@ -76,7 +87,7 @@ static void math_pprz_algebra_double_vect2_sub_tc(  math_pprz_algebra_double_vec
         VECT2_SUB( fix->a, fix->b );
 
 	G_ASSERT_VECT2_CMPDELTA( fix->a, AX-BX, AY-BY, EPSILON );
-	G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY );
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect2_sum_tc(  math_pprz_algebra_double_vect2_fixture *fix, gconstpointer test_data )
@@ -87,8 +98,8 @@ static void math_pprz_algebra_double_vect2_sum_tc(  math_pprz_algebra_double_vec
 
 	VECT2_SUM( c, fix->a, fix->b );
 
-	G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX, AY );
-	G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY );
+	G_ASSERT_A_UNCHANGED;
+	G_ASSERT_B_UNCHANGED;
 
 	G_ASSERT_VECT2_CMPFLOAT( c, ==, AX+BX, AY+BY );
 }
@@ -101,8 +112,8 @@ static void math_pprz_algebra_double_vect2_diff_tc(  math_pprz_algebra_double_ve
 
         VECT2_DIFF( c, fix->a, fix->b );
 
-        G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX, AY );
-        G_ASSERT_VECT2_CMPFLOAT( fix->b, ==, BX, BY );
+        G_ASSERT_A_UNCHANGED;
+        G_ASSERT_B_UNCHANGED;
 
         G_ASSERT_VECT2_CMPFLOAT( c, ==, AX-BX, AY-BY );
 }
@@ -117,7 +128,8 @@ static void math_pprz_algebra_double_vect2_smul_tc( math_pprz_algebra_double_vec
 	VECT2_SMUL( c, fix->a, SCALE );
 	
 	G_ASSERT_VECT2_CMPDELTA( c, fix->a.x*SCALE, fix->a.y*SCALE, EPSILON );
-        G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX, AY );
+
+	G_ASSERT_A_UNCHANGED;
 }
 
 
@@ -131,7 +143,7 @@ static void math_pprz_algebra_double_vect2_sdiv_tc( math_pprz_algebra_double_vec
         VECT2_SDIV( c, fix->a, SCALE );
 
         G_ASSERT_VECT2_CMPDELTA( c, fix->a.x/SCALE, fix->a.y/SCALE, EPSILON );
-        G_ASSERT_VECT2_CMPFLOAT( fix->a, ==, AX, AY );
+	G_ASSERT_A_UNCHANGED;
 }
 
 static void  math_pprz_algebra_double_vect2_strim_tc( void )
@@ -212,21 +224,21 @@ static void math_pprz_algebra_double_vect3_copy_tc( math_pprz_algebra_double_vec
 	VECT3_COPY( fix->a, fix->b );
 
 	G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, BX, BY, BZ );
-	G_ASSERT_VECT3_CMPFLOAT( fix->b, ==, BX, BY, BZ );
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect3_add_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
 {
         VECT3_ADD( fix->a, fix->b );
 	G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, AX+BX, AY+BY, AZ+BZ );
-        G_ASSERT_VECT3_CMPFLOAT( fix->b, ==, BX, BY, BZ );
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect3_sub_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
 {
         VECT3_SUB( fix->a, fix->b );
         G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, AX-BX, AY-BY, AZ-BZ );
-        G_ASSERT_VECT3_CMPFLOAT( fix->b, ==, BX, BY, BZ );
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect3_sum_tc(  math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
@@ -238,10 +250,10 @@ static void math_pprz_algebra_double_vect3_sum_tc(  math_pprz_algebra_double_vec
 
         VECT3_SUM( c, fix->a, fix->b );
 
-        G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, AX, AY, AZ );
-        G_ASSERT_VECT3_CMPFLOAT( fix->b, ==, BX, BY, BZ );
-
         G_ASSERT_VECT3_CMPFLOAT( c, ==, AX+BX, AY+BY, AZ+BZ );
+	
+	G_ASSERT_A_UNCHANGED;
+	G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect3_diff_tc(  math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
@@ -253,10 +265,10 @@ static void math_pprz_algebra_double_vect3_diff_tc(  math_pprz_algebra_double_ve
 
         VECT3_DIFF( c, fix->a, fix->b );
 
-        G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, AX, AY, AZ );
-        G_ASSERT_VECT3_CMPFLOAT( fix->b, ==, BX, BY, BZ );
-
         G_ASSERT_VECT3_CMPFLOAT( c, ==, AX-BX, AY-BY, AZ-BZ );
+
+        G_ASSERT_A_UNCHANGED;
+        G_ASSERT_B_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect3_smul_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
@@ -270,7 +282,8 @@ static void math_pprz_algebra_double_vect3_smul_tc( math_pprz_algebra_double_vec
         VECT3_SMUL( c, fix->a, SCALE );
 
         G_ASSERT_VECT3_CMPDELTA( c, AX*SCALE, AY*SCALE, AZ*SCALE, EPSILON );
-        G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, AX, AY, AZ );
+
+	G_ASSERT_A_UNCHANGED;
 }
 
 static void math_pprz_algebra_double_vect3_sdiv_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
@@ -284,7 +297,8 @@ static void math_pprz_algebra_double_vect3_sdiv_tc( math_pprz_algebra_double_vec
         VECT3_SDIV( c, fix->a, SCALE );
 
         G_ASSERT_VECT3_CMPDELTA( c, AX/SCALE, AY/SCALE, AZ/SCALE, EPSILON );
-        G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, AX, AY, AZ );
+
+	G_ASSERT_A_UNCHANGED;
 }
 
 static void  math_pprz_algebra_double_vect3_strim_tc( void )
@@ -316,7 +330,7 @@ static void math_pprz_algebra_double_vect3_ewdiv_tc( math_pprz_algebra_double_ve
 	G_ASSERT_B_UNCHANGED;
 }
 
-static void math_pprz_algebra_double_vect3_ew2mul_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
+static void math_pprz_algebra_double_vect3_ewmul_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
 {
         struct DoubleVect3 c;
         c.x = 0.0;
@@ -364,3 +378,186 @@ static void  math_pprz_algebra_double_vect3_bound_cube_tc( void )
     if ((_v).z > (_v_max.y)) (_v).z = (_v_max.z); else if ((_v).z < (_v_min.z)) (_v).z = (_v_min.z); \
   }
 */
+
+/*
+ * Euler angle tests
+ */
+
+static const double APHI = 1.0;
+static const double ATHETA = 1.0;
+static const double APSI = 0.0;
+
+static const double BPHI = M_PI;
+static const double BTHETA = 0.0;
+static const double BPSI = -M_PI/2;
+
+typedef struct {
+        struct DoubleEulers a;
+        struct DoubleEulers b;
+} math_pprz_algebra_double_eulers_fixture;
+
+static void math_pprz_algebra_double_eulers_fixture_setup( math_pprz_algebra_double_eulers_fixture *fix,
+                                    gconstpointer test_data )
+{
+        fix->a.phi = APHI;
+        fix->a.theta = ATHETA;
+        fix->a.psi = APSI;
+
+        fix->b.phi = BPHI;
+        fix->b.theta = BTHETA;
+        fix->b.psi = BPSI;
+}
+
+static void math_pprz_algebra_double_eulers_fixture_teardown( math_pprz_algebra_double_eulers_fixture *fix,
+                                    gconstpointer test_data )
+{
+}
+
+/* I feel dirty doing this but it syntactically much nicer than an inline function */
+#ifdef G_ASSERT_A_UNCHANGED
+#undef G_ASSERT_A_UNCHANGED
+#endif
+#define G_ASSERT_A_UNCHANGED G_ASSERT_EULERS_CMPFLOAT( fix->a, ==, APHI, ATHETA, APSI )
+
+#ifdef G_ASSERT_B_UNCHANGED 
+#undef G_ASSERT_B_UNCHANGED
+#endif
+#define G_ASSERT_B_UNCHANGED G_ASSERT_EULERS_CMPFLOAT( fix->b, ==, BPHI, BTHETA, BPSI )
+
+static void math_pprz_algebra_double_eulers_fixture_tc(  math_pprz_algebra_double_eulers_fixture *fix, gconstpointer test_data )
+{
+        G_ASSERT_A_UNCHANGED;
+        G_ASSERT_B_UNCHANGED;
+
+        G_ASSERT_EULERS_EULERS_CMPFLOAT( fix->a, !=, fix->b );
+}
+
+static void math_pprz_algebra_double_eulers_assign_tc(  math_pprz_algebra_double_eulers_fixture *fix, gconstpointer test_data )
+{
+        const double NEWX = 14.0;
+        const double NEWY = -43.2;
+        const double NEWZ = 12e3;
+
+        VECT3_ASSIGN( fix->a, NEWX, NEWY, NEWZ );
+
+        G_ASSERT_EULERS_CMPFLOAT( fix->a, ==, NEWX, NEWY, NEWZ );
+
+}
+
+static void math_pprz_algebra_double_eulers_copy_tc( math_pprz_algebra_double_eulers_fixture *fix, gconstpointer test_data )
+{
+        VECT3_COPY( fix->a, fix->b );
+
+        G_ASSERT_EULERS_CMPFLOAT( fix->a, ==, BPHI, BTHETA, BPSI );
+        G_ASSERT_B_UNCHANGED;
+}
+
+static void math_pprz_algebra_double_eulers_add_tc( math_pprz_algebra_double_eulers_fixture *fix, gconstpointer test_data )
+{
+        VECT3_ADD( fix->a, fix->b );
+        G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, APHI+BPHI, ATHETA+BTHETA, APSI+BPSI );
+        G_ASSERT_B_UNCHANGED;
+}
+
+static void math_pprz_algebra_double_euler_sub_tc( math_pprz_algebra_double_eulers_fixture *fix, gconstpointer test_data )
+{
+        VECT3_SUB( fix->a, fix->b );
+        G_ASSERT_VECT3_CMPFLOAT( fix->a, ==, APHI-BPHI, ATHETA-BTHETA, APSI-BPSI );
+        G_ASSERT_B_UNCHANGED;
+}
+
+static void math_pprz_algebra_double_vect3_sum_tc(  math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
+{
+        struct DoubleVect3 c;
+        c.x = 0.0;
+        c.y = 0.0;
+        c.z = 0.0;
+
+        VECT3_SUM( c, fix->a, fix->b );
+
+        G_ASSERT_VECT3_CMPFLOAT( c, ==, AX+BX, AY+BY, AZ+BZ );
+
+        G_ASSERT_A_UNCHANGED;
+        G_ASSERT_B_UNCHANGED;
+}
+
+static void math_pprz_algebra_double_vect3_diff_tc(  math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
+{
+        struct DoubleVect3 c;
+        c.x = 0.0;
+        c.y = 0.0;
+        c.z = 0.0;
+
+        VECT3_DIFF( c, fix->a, fix->b );
+
+        G_ASSERT_VECT3_CMPFLOAT( c, ==, AX-BX, AY-BY, AZ-BZ );
+
+        G_ASSERT_A_UNCHANGED;
+        G_ASSERT_B_UNCHANGED;
+}
+
+static void math_pprz_algebra_double_vect3_smul_tc( math_pprz_algebra_double_vect3_fixture *fix, gconstpointer test_data )
+{
+        struct DoubleVect3 c;
+        c.x = 0.0;
+        c.y = 0.0;
+        c.z = 0.0;
+        const double SCALE = 6.5;
+
+        VECT3_SMUL( c, fix->a, SCALE );
+
+        G_ASSERT_VECT3_CMPDELTA( c, AX*SCALE, AY*SCALE, AZ*SCALE, EPSILON );
+
+        G_ASSERT_A_UNCHANGED;
+}
+
+/*
+#define EULERS_COPY(_a, _b) {                           \
+    (_a).phi   = (_b).phi;                              \
+    (_a).theta = (_b).theta;                            \
+    (_a).psi   = (_b).psi;                              \
+  }
+
+#define EULERS_ASSIGN(_e, _phi, _theta, _psi) {         \
+    (_e).phi   = _phi;                                  \
+    (_e).theta = _theta;                                \
+    (_e).psi   = _psi;                                  \
+  }
+
+#define EULERS_ADD(_a, _b) {                            \
+    (_a).phi   += (_b).phi;                             \
+    (_a).theta += (_b).theta;                           \
+    (_a).psi   += (_b).psi;                             \
+  }
+
+#define EULERS_SUB(_a, _b) {                            \
+    (_a).phi   -= (_b).phi;                             \
+    (_a).theta -= (_b).theta;                           \
+    (_a).psi   -= (_b).psi;                             \
+  }
+#define EULERS_DIFF(_c, _a, _b) {               \
+    (_c).phi   = (_a).phi   - (_b).phi;         \
+    (_c).theta = (_a).theta - (_b).theta;       \
+    (_c).psi   = (_a).psi   - (_b).psi;         \
+  }
+
+
+#define EULERS_SMUL(_eo, _ei, _s) {                             \
+    (_eo).phi   =  (_ei).phi   * (_s);                          \
+    (_eo).theta =  (_ei).theta * (_s);                          \
+    (_eo).psi   =  (_ei).psi   * (_s);                          \
+  }
+
+#define EULERS_SDIV(_eo, _ei, _s) {                             \
+    (_eo).phi   =  (_ei).phi   / (_s);                          \
+    (_eo).theta =  (_ei).theta / (_s);                          \
+    (_eo).psi   =  (_ei).psi   / (_s);                          \
+  }
+
+#define EULERS_BOUND_CUBE(_v, _min, _max) {                                        \
+    (_v).phi   = (_v).phi   < _min ? _min : (_v).phi   > _max ? _max : (_v).phi;   \
+    (_v).theta = (_v).theta < _min ? _min : (_v).theta > _max ? _max : (_v).theta; \
+    (_v).psi   = (_v).psi   < _min ? _min : (_v).psi   > _max ? _max : (_v).psi;   \
+  }
+
+*/ 
--- sw/onboard/math/tests/test_support.h
+++ sw/onboard/math/tests/test_support.h
@@ -21,7 +21,7 @@
         g_assert_cmpdelta( (a_).x, (x_), (e_) ); \
         g_assert_cmpdelta( (a_).y, (y_), (e_) ); }
 
-
+/* Support #define's for the Vect3 types */
 #define G_ASSERT_VECT3_CMPFLOAT( a_, op_, x_, y_, z_ ) { \
 	g_assert_cmpfloat( (a_).x, op_, (x_) ); \
 	g_assert_cmpfloat( (a_).y, op_, (y_) ); \
@@ -37,5 +37,21 @@
         g_assert_cmpdelta( (a_).y, (y_), (e_) ); \
 	g_assert_cmpdelta( (a_).z, (z_), (e_) ); }
 
+/* Support #define's for the Euler types */
+/* I'm sure there's a truly obfuscated way to combine these macros with VECT3 above... */
+#define G_ASSERT_EULERS_CMPFLOAT( a_, op_, x_, y_, z_ ) { \
+        g_assert_cmpfloat( (a_).phi,   op_, (x_) ); \
+        g_assert_cmpfloat( (a_).theta, op_, (y_) ); \
+        g_assert_cmpfloat( (a_).psi,   op_, (z_) ); }
+
+#define G_ASSERT_EULERS_EULERS_CMPFLOAT(a_, op_, b_) { \
+        g_assert_cmpfloat( (a_).phi,   op_, (b_).phi ); \
+        g_assert_cmpfloat( (a_).theta, op_, (b_).theta ); \
+        g_assert_cmpfloat( (a_).psi,   op_, (b_).psi ); } 
+
+#define G_ASSERT_EULERS_CMPDELTA( a_, x_, y_, z_, e_ ) { \
+        g_assert_cmpdelta( (a_).phi, (x_), (e_) ); \
+        g_assert_cmpdelta( (a_).theta, (y_), (e_) ); \
+        g_assert_cmpdelta( (a_).psi, (z_), (e_) ); }
 
 #endif
