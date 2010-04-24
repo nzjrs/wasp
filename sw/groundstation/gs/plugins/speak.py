4266517143ee2067139e59e88e017322494039f3
John Stowers <john.stowers@gmail.com>
2010-04-25 00:26:16 +1200

    [gs] watch for low battery voltage

--- sw/groundstation/gs/plugins/speak.py
+++ sw/groundstation/gs/plugins/speak.py
@@ -15,7 +15,9 @@ class Speak(plugin.Plugin, config.ConfigurableIface):
         config.ConfigurableIface.__init__(self, conf)
         self.autobind_config("enabled")
         self.process = None
+
         source.connect("source-connected", self._source_connected)
+        source.register_interest(self._on_status, 1, "STATUS")
 
     def _speak(self, msg):
         if self._enabled != "1":
@@ -40,6 +42,20 @@ class Speak(plugin.Plugin, config.ConfigurableIface):
             self._speak("connected ok")
         elif status == source.STATUS_DISCONNECTED:
             self._speak("disconnected")
+
+    def _on_status(self, msg, header, payload):
+        rc, gps, bv, in_flight, motors_on, autopilot_mode, cpu_usage = msg.unpack_values(payload)
+        say = []
+
+        #check battery voltage (in decivolts)
+        if bv < 100:
+            say.append("low battery")
+        #check RC is connected
+        if rc != msg.get_field_by_name("rc").interpret_value_from_user_string("OK"):
+            say.append("r c lost")
+
+        if say:
+            self._speak(".\n".join(say))
 
     def get_preference_widgets(self):
         e = self.build_checkbutton("enabled")
