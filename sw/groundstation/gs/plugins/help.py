cffb85fa0763b32bb797b3521c18838241968d15
John Stowers <john.stowers@gmail.com>
2010-05-05 19:03:24 +1200

    [gs] add plugin to link to online docs

---
+++ sw/groundstation/gs/plugins/help.py
@@ -0,0 +1,19 @@
+import gtk
+
+import gs.plugin as plugin
+
+class Help(plugin.Plugin):
+
+    def __init__(self, conf, source, messages_file, groundstation_window):
+        self._add_link(groundstation_window, 
+                "Wasp Documentation",
+                "http://open.grcnz.com/downloads/wasp/doc/")
+
+    def _add_link(self, groundstation_window, name, url):
+        item = gtk.ImageMenuItem(name)
+        item.set_image(gtk.image_new_from_stock(gtk.STOCK_HELP, gtk.ICON_SIZE_MENU))
+        item.connect("activate", self._show_url, url)
+        groundstation_window.add_menu_item("Help", item)
+
+    def _show_url(self, menuitem, url):
+        gtk.show_uri(None, url, gtk.gdk.CURRENT_TIME)
