41b6fe98b8168554ba9a2382c27ecddcd7a44d9c
John Stowers <john.stowers@gmail.com>
2010-06-02 23:34:47 +1200

    [gs] Link to my site for the docs

--- sw/groundstation/gs/plugins/help.py
+++ sw/groundstation/gs/plugins/help.py
@@ -7,7 +7,7 @@ class Help(plugin.Plugin):
     def __init__(self, conf, source, messages_file, groundstation_window):
         self._add_link(groundstation_window, 
                 "Wasp Documentation",
-                "http://open.grcnz.com/downloads/wasp/doc/")
+                "http://www.johnstowers.co.nz/files/wasp/")
 
     def _add_link(self, groundstation_window, name, url):
         item = gtk.ImageMenuItem(name)
