e6e5c821a937102bccc7d53f7482ef3d1f63fb36
John Stowers <john.stowers@gmail.com>
2010-07-20 16:17:37 +1200

    [ob] FMS plumbing

---
+++ sw/onboard/fms.c
@@ -0,0 +1,14 @@
+#include "fms.h"
+
+FMS_t fms_state;
+SystemStatus_t fms_system_status = STATUS_UNINITIAIZED;
+
+void fms_init(void)
+{
+
+}
+
+void fms_periodic_task(void)
+{
+
+}
---
+++ sw/onboard/fms.h
@@ -0,0 +1,36 @@
+#ifndef _FMS_H_
+#define _FMS_H_
+
+#include "std.h"
+#include "math/pprz_algebra_int.h"
+
+typedef struct __FMSCommand {
+    union {
+        struct Int32Vect3   rate;
+        struct Int32Eulers  attitude;
+        struct Int32Vect2   speed;
+        struct Int32Vect3   pos;          //FIXME Warning z is heading
+    } h_sp;
+    union {
+        int32_t direct;
+        int32_t climb;
+        int32_t height;
+    } v_sp;
+    uint8_t h_mode;
+    uint8_t v_mode;
+} FMSCommand_t;
+
+typedef struct __FMS {
+    FMSCommand_t    command;
+    bool_t          enabled;
+    bool_t          timeout;
+    uint8_t         last_msg;
+} FMS_t;
+
+extern FMS_t            fms_state;
+extern SystemStatus_t   fms_system_status;
+
+void fms_init(void);
+void fms_periodic_task(void);
+
+#endif /* _FMS_H_ */
