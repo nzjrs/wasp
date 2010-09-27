77bf67559461a4796fed8fa795ca557253248ef1
John Stowers <john.stowers@gmail.com>
2010-09-27 15:39:43 +1300

    [gs][doc] Upload to wasp website

--- sw/groundstation/gs/plugins/help.py
+++ sw/groundstation/gs/plugins/help.py
@@ -7,7 +7,7 @@ class Help(plugin.Plugin):
     def __init__(self, conf, source, messages_file, groundstation_window):
         self._add_link(groundstation_window, 
                 "Wasp Documentation",
-                "http://www.johnstowers.co.nz/files/wasp/")
+                "http://www.waspuav.org/doc/")
 
     def _add_link(self, groundstation_window, name, url):
         item = gtk.ImageMenuItem(name)
